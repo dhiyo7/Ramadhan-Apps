33b74d1a5711b0cb2d6b951654597e1a95e75c72
dhiyo7 <dhiyoreksa12@gmail.com>
2019-05-25 17:32:58 +0700

    UI Doang

---
+++ app/src/main/java/dev7/id/ramadhanapps/MainActivity.java
@@ -0,0 +1,15 @@
+package dev7.id.ramadhanapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+ import dev7.id.ramadhanapps.R;
+
+ public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/dev7/id/ramadhanapps/Splash.java
@@ -0,0 +1,29 @@
+package dev7.id.ramadhanapps;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    private int loading=2000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home=new Intent(Splash.this, MainActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        },loading);
+
+    }
+}
